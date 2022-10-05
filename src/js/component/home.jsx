d01a604273e9b9c750bc74d1c61fa5f684a9e429
federicaramirez <federicaaramirez@gmail.com>
2022-10-05 17:25:11 +0000

    keesesto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,13 +10,20 @@ const Semaforo = () => {
 	const [shadow, useShadow] = useState("");
 	console.log(shadow);
 	
+const encenderLuz= (event) => {
+	if ( shadow == "") {
+		useShadow("shadow-lg")		
+	} 
+
+}
+
 	return (
 		<div className="position-absolute top-50 start-50 translate-middle">
-		<div class="container text-center display-flex">
-  <div class="row row-cols-2 bg-black">
-    <div class="col-12 rounded-circle bg-danger">Column</div>
-    <div class="col-12 rounded-circle bg-warning">Column</div>
-    <div class="col-12 rounded-circle bg-success" >Column</div>
+		<div className="container text-center display-flex">
+  <div className="row row-cols-2 bg-black">
+    <div className={"col-12 rounded-circle bg-danger" + shadow}  onClick= {()=> encenderLuz()}  ></div>
+    <div className={"col-12 rounded-circle bg-warning " + shadow} >Column</div>
+    <div className={"col-12 rounded-circle bg-success " + shadow}>Column</div>
   </div>
 </div>
 </div>
