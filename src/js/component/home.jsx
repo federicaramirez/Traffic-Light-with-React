0963e9d5b9ecc8d7c60aef875f5e10697097cc02
federicaramirez <federicaaramirez@gmail.com>
2022-10-05 16:26:10 +0000

    comentario

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+// import React ,  {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Semaforo = () => {
+	
+	const [shadow, useShadow] = useState("");
+	console.log(shadow);
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="position-absolute top-50 start-50 translate-middle">
+		<div class="container text-center display-flex">
+  <div class="row row-cols-2 bg-black">
+    <div class="col-12 rounded-circle bg-danger">Column</div>
+    <div class="col-12 rounded-circle bg-warning">Column</div>
+    <div class="col-12 rounded-circle bg-success" >Column</div>
+  </div>
+</div>
+</div>
 	);
 };
 
-export default Home;
+export default Semaforo;
